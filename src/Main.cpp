d1e63423dd3d02ca1c69f2ec06caae584006d4bf
arthurhenrique <arthur393@gmail.com>
2016-06-04 01:55:34 +0100

    changed main

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,11 +10,10 @@ int main(int argc, char **argv)
     string name = "Arthur";
     Person *person = new Person(name);
     
-    cout << person->GetName() << endl;
-
-    cout << person->GetBirthday().day << endl;
-    cout << person->GetBirthday().month << endl;
-    cout << person->GetBirthday().year << endl;
+    cout << "GetName:     " << person->GetName()           << endl;
+    cout << "GetBirthday: " << person->GetBirthday().day   << endl;
+    cout << "GetBirthday: " << person->GetBirthday().month << endl;
+    cout << "GetBirthday: " << person->GetBirthday().year  << endl;
 
     delete person;
 
